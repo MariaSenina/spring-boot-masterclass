3246c99fd11fd32c01e06ad421078db397f1dee0
Maria <maria.senina15@gmail.com>
2022-04-23 19:09:51 -0400

    Tight coupling with sorting algorithms

---
+++ spring-basics/src/main/java/com/senina/maria/springbasics/BinarySearchImpl.java
@@ -0,0 +1,15 @@
+package com.senina.maria.springbasics;
+
+public class BinarySearchImpl {
+
+    public int binarySearch(int[] numbers, int numberToSearchFor) {
+        // Sort an array
+        BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+        int[] sortedNumbers = bubbleSortAlgorithm.sort(numbers);
+
+        // Search the array
+
+        // Return the result
+        return 3;
+    }
+}
---
+++ spring-basics/src/main/java/com/senina/maria/springbasics/BubbleSortAlgorithm.java
@@ -0,0 +1,9 @@
+package com.senina.maria.springbasics;
+
+public class BubbleSortAlgorithm {
+    public int[] sort(int[] numbers) {
+        // Logic for Bubble Sort
+
+        return numbers;
+    }
+}
