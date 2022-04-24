33a2064769aaff7f9d3eeb7696eda8108b595129
Maria <maria.senina15@gmail.com>
2022-04-24 18:59:18 -0400

    Loose coupling using Spring

--- spring-basics/src/main/java/com/senina/maria/springbasics/BubbleSortAlgorithm.java
+++ spring-basics/src/main/java/com/senina/maria/springbasics/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.senina.maria.springbasics;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
     @Override
     public int[] sort(int[] numbers) {
--- spring-basics/src/main/java/com/senina/maria/springbasics/SpringBasicsApplication.java
+++ spring-basics/src/main/java/com/senina/maria/springbasics/SpringBasicsApplication.java
@@ -2,15 +2,18 @@ package com.senina.maria.springbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(new BubbleSortAlgorithm());
+
+		// Application Context
+		ApplicationContext applicationContext = SpringApplication.run(SpringBasicsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
 		System.out.println(result);
-
-		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
 }
