b7d59c733e15babb1d3df08e81566b9be460a21e
Maria <maria.senina15@gmail.com>
2022-04-26 15:52:32 -0400

    @PostConstruct and @PreDestroy annotations

--- spring-basics/src/main/java/com/senina/maria/springbasics/basic/BinarySearchImpl.java
+++ spring-basics/src/main/java/com/senina/maria/springbasics/basic/BinarySearchImpl.java
@@ -1,15 +1,20 @@
 package com.senina.maria.springbasics.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-// @Scope("prototype")
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     @Qualifier("quick")
@@ -28,5 +33,16 @@ public class BinarySearchImpl {
 
         // Return the result
         return 3;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        logger.info("Post-Construct");
+    }
+
+    // PreDestroy is only called with singleton scope.
+    @PreDestroy
+    public void preDestroy() {
+        logger.info("Pre-Destroy");
     }
 }
