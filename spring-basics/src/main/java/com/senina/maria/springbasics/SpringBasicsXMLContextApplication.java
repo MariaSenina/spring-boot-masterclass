d34fa1bd3f2deef47f6ff344eaf9ede2e86e3b93
Maria <maria.senina15@gmail.com>
2022-04-27 18:07:11 -0400

    Refactor (remove remaining Spring annotations)

--- spring-basics/src/main/java/com/senina/maria/springbasics/SpringBasicsXMLContextApplication.java
+++ spring-basics/src/main/java/com/senina/maria/springbasics/SpringBasicsXMLContextApplication.java
@@ -3,17 +3,12 @@ package com.senina.maria.springbasics;
 import com.senina.maria.springbasics.xml.XmlPersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringBasicsXMLContextApplication {
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringBasicsXMLContextApplication.class);
 
 	public static void main(String[] args) {
-
-		Logger LOGGER = LoggerFactory.getLogger(SpringBasicsXMLContextApplication.class);
 
 		// Application Context
 		// Auto-close using "try"
