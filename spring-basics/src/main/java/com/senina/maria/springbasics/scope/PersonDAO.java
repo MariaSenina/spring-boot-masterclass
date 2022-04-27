3a11c9a8f61b84754fdd75d50863dd291d2ffb0e
Maria <maria.senina15@gmail.com>
2022-04-27 18:47:44 -0400

    Update component annotations to reflect their purpose

--- spring-basics/src/main/java/com/senina/maria/springbasics/scope/PersonDAO.java
+++ spring-basics/src/main/java/com/senina/maria/springbasics/scope/PersonDAO.java
@@ -1,9 +1,9 @@
 package com.senina.maria.springbasics.scope;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public class PersonDAO {
     @Autowired
     private JdbcConnection jdbcConnection;
