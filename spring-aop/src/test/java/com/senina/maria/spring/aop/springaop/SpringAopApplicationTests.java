41e1e4522d537c390e9c2a09cda0be2e30db1670
Maria <maria.senina15@gmail.com>
2022-04-29 15:21:52 -0400

    Initialize Spring AOP Project

---
+++ spring-aop/src/test/java/com/senina/maria/spring/aop/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senina.maria.spring.aop.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
