19b15f0cc8a7d455265bd7cd6512b9aac05496ff
Maria <maria.senina15@gmail.com>
2022-04-29 17:31:40 -0400

    Other @Pointcut options

--- spring-aop/src/main/java/com/senina/maria/spring/aop/springaop/aspect/CommonJoinPointConfig.java
+++ spring-aop/src/main/java/com/senina/maria/spring/aop/springaop/aspect/CommonJoinPointConfig.java
@@ -5,4 +5,17 @@ import org.aspectj.lang.annotation.Pointcut;
 public class CommonJoinPointConfig {
     @Pointcut("execution(* com.senina.maria.spring.aop.springaop.business.*.*(..))")
     public void businessLayerExecution() {}
+
+    @Pointcut("execution(* com.senina.maria.spring.aop.springaop.data.*.*(..))")
+    public void dataLayerExecution() {}
+
+    @Pointcut("com.senina.maria.spring.aop.springaop.aspect.CommonJoinPointConfig.businessLayerExecution() " +
+            "&& com.senina.maria.spring.aop.springaop.aspect.CommonJoinPointConfig.dataLayerExecution{}")
+    public void allLayerExecution() {}
+
+    @Pointcut("bean(*dao*)")
+    public void beanContainingDao(){}
+
+    @Pointcut("within(com.senina.maria.spring.aop.springaop.data..*)")
+    public void dataLayerExecutionWithWithin() {}
 }
