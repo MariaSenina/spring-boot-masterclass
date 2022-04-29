75c5536d8465817d4beea2ae5618583620c490e4
Maria <maria.senina15@gmail.com>
2022-04-29 16:28:15 -0400

    Defining an example of @Before advice

---
+++ spring-aop/src/main/java/com/senina/maria/spring/aop/springaop/aspect/UserAccessAspect.java
@@ -0,0 +1,23 @@
+package com.senina.maria.spring.aop.springaop.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class UserAccessAspect {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    // What do we want to intercept?
+    // execution(* PACKAGE.*.*(..))
+    @Before("execution(* com.senina.maria.spring.aop.springaop.business.*.*(..))")
+    public void before(JoinPoint joinPoint) {
+        logger.info("Checking user access level");
+        logger.info("Allowed {}", joinPoint);
+    }
+}
