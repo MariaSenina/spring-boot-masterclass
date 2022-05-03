0fc9e21738d1eb2abe92271a0da65622c44cd2b2
Maria <maria.senina15@gmail.com>
2022-05-03 13:33:46 -0400

    Initialize database-demo project

---
+++ database-demo/src/test/java/com/senina/maria/database/databasedemo/DatabaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senina.maria.database.databasedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
