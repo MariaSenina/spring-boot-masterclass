1a56bddd42e156331d909ff277dc5068988d6e90
Maria <maria.senina15@gmail.com>
2022-05-03 14:41:09 -0400

    JDBC: Implement findAll() method (+ refactor)

---
+++ database-demo/src/main/java/com/senina/maria/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -0,0 +1,20 @@
+package com.senina.maria.database.databasedemo.jdbc;
+
+import com.senina.maria.database.databasedemo.entity.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PersonJdbcDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // SELECT * FROM person
+    public List<Person> findAll() {
+        return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper<>(Person.class));
+    }
+}
