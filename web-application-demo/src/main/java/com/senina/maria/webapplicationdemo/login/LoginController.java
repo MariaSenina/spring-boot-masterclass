76d4596d07eaa440655765dcc655924f5aa0e99b
Maria <maria.senina15@gmail.com>
2022-05-13 15:00:47 -0400

    Add @Autowired and @Service annotations

--- web-application-demo/src/main/java/com/senina/maria/webapplicationdemo/login/LoginController.java
+++ web-application-demo/src/main/java/com/senina/maria/webapplicationdemo/login/LoginController.java
@@ -1,5 +1,6 @@
 package com.senina.maria.webapplicationdemo.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
-    private UserValidationService validationService = new UserValidationService();
+    @Autowired
+    private UserValidationService validationService;
 
     @RequestMapping(value="login", method = RequestMethod.GET)
     public String showLoginPage() {
--- web-application-demo/src/main/java/com/senina/maria/webapplicationdemo/login/UserValidationService.java
+++ web-application-demo/src/main/java/com/senina/maria/webapplicationdemo/login/UserValidationService.java
@@ -1,5 +1,8 @@
 package com.senina.maria.webapplicationdemo.login;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserValidationService {
     public boolean isValidUser(String userName, String password) {
         return userName.equals("Mashimaro") && password.equals("test");
